7b3ce07fd5ac51940b24a140d642eccd6d51ed05
pchitanta <patrick.chitanta@quinscape.de>
2021-11-02 18:12:30 +0100

    First Try

--- src/de/quinscape/Gluecksrad.java
+++ src/de/quinscape/Gluecksrad.java
@@ -1,5 +1,26 @@
 package de.quinscape;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Gluecksrad {
-    public String retrieveUserInput;
+    public String retrieveUserInput(){
+        Scanner userInput = new Scanner(System.in);
+        return userInput.nextLine();
+    }
+
+    public void pickRandomWord (List<String> words){
+
+    }
+
+    public void wordArray(){
+        String[] cityArray = {"hamburg", "dortmund", "berlin", "düsseldorf", "münchen", "münster"};
+    }
+
+    public static String getRandom(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
 }
--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
-
+        Gluecksrad test = new Gluecksrad();
+        //test.retrieveUserInput();
     }
 
 
