05b192ec04fe08684c0632833d9688b26bc7fd2f
pchitanta <patrick.chitanta@quinscape.de>
2021-11-03 13:41:21 +0100

    Idea for tomorrow added: triesLeft userInput if statement in random

--- src/de/quinscape/Gluecksrad.java
+++ src/de/quinscape/Gluecksrad.java
@@ -1,11 +1,8 @@
 package de.quinscape;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Random;
-
 
 public class Gluecksrad {
     public static LinkedList<String> cityList() {
@@ -23,21 +20,29 @@ public class Gluecksrad {
         Random randomGenerator = new Random();
         String randomCity = cityList().get(randomGenerator.nextInt(cityList().size()));
         System.out.println(randomCity);
+
         if(randomCity != userInput()){
-          triesLeft();
+          triesLeft(6);
+          userInput();
         }
 
     }
 
-    public static void triesLeft(){
-      for(int i = 6; i > 0; i--){
+    public void triesLeft(int number){
+      for(int i = number; i > 0; i--){
         System.out.println("You have " + i + " tries left");
       }
     }
 
     public String userInput(){
       Scanner input = new Scanner(System.in);
-      System.out.println("Enter city name");
+      for(int i = 6; i > 0; i--){
+          System.out.println("Enter city name (" + i + " tries left)");
+//          if(input.nextLine()==){
+//              input.nextLine();
+//          }
+
+      }
       return input.nextLine();
     }
 
--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -1,14 +1,11 @@
 package de.quinscape;
 
-import java.util.Scanner;
-import java.util.Random;
-
 
 public class Main {
     public static void main(String[] args) {
         Gluecksrad gr = new Gluecksrad();
         gr.random();
-
+        //gr.triesLeft(6);
 
 
     }
