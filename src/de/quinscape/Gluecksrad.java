370d219ed3b2f73a65146c2d59c99092ded0c327
pchitanta <patrick.chitanta@quinscape.de>
2021-11-04 18:12:54 +0100

    Code update: - added: new methods, TODO tasks and Getter & Setter - code contains errors:   - cityStringToCharArray not finished yet

--- src/de/quinscape/Gluecksrad.java
+++ src/de/quinscape/Gluecksrad.java
@@ -1,10 +1,15 @@
 package de.quinscape;
 
-import java.util.LinkedList;
-import java.util.Random;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Gluecksrad {
+    private Random randomGenerator = new Random();
+
+    private String currentCity;
+    //private boolean foundChar;
+    //private int currentAttempt -- hinzufügen wenn es soweit ist
+
     public static LinkedList<String> cityList() {
         LinkedList<String> cities = new LinkedList<>();
         cities.add("Dortmund");
@@ -16,34 +21,129 @@ public class Gluecksrad {
         return cities;
     }
 
-    public void random() {
-        Random randomGenerator = new Random();
-        String randomCity = cityList().get(randomGenerator.nextInt(cityList().size()));
-        System.out.println(randomCity);
+    public String randomCityGenerator() {
+        return cityList().get(randomGenerator.nextInt(cityList().size()));
+    }
 
-        if(randomCity != userInput()){
-          triesLeft(6);
-          userInput();
+    public void createRandomCity() {
+        setCurrentCity(randomCityGenerator());
+    }
+
+    public void userInput() {
+        Scanner input = new Scanner(System.in);
+        roundCounter(input);
+    }
+
+    public void roundCounter(Scanner input) {
+        boolean done = false;
+        int triesLeft = getCurrentCity().length() * 2;
+        System.out.println("You have " + triesLeft + " rounds to guess the right word.");
+
+        int round = 0;
+        while (!done) {
+            round++;
+            System.out.println("Round " + round);
+            String line = input.nextLine();
+            char letter = line.charAt(0);
+            System.out.println(getCurrentCity().indexOf(letter));
+
+            if (round == triesLeft) {
+                input.close();
+                System.out.println("Game over. No more tries left");
+                done = true;
+                //           } else if(wordGuessed){
+//
+//
+//                done = true;
+//                System.out.println("You win!");
+//            }
+            }
         }
+    }
+    //      TODO isGameOver Methode
 
+    public void printCurrentCity() {
+        System.out.println("Current city is: " + getCurrentCity());
     }
 
-    public void triesLeft(int number){
-      for(int i = number; i > 0; i--){
-        System.out.println("You have " + i + " tries left");
-      }
+//          TODO: Liste erstellen um die richtig geratenen Buchstaben zu speichern
+//          TODO: emptyLineWord updaten wenn buchstabe erraten wird
+
+
+//    public String wordWithPlaceholder(String currentWord, List<String> gussedCharacters){
+//        //TODO: die funktion solld as currentWordd ausgeben, nur mit den buchstaben die richtig geraten sind
+//        //TODO: Stringbuilder baut strings (stringbuilder.apend) return
+//
+//    }
+
+
+    public char[] cityStringToCharArray() {
+
+        String currentCity = getCurrentCity();
+        int stringLength = currentCity.length();
+        char[] charArray = new char[stringLength];
+
+        for (int i = 0; i < stringLength; i++) {
+            if(currentCity.contains(charArray[i]){ //wenn currentCity "Berlin" das element i vom charArray hat --> dann
+                charArray[i] = currentCity.charAt(i); // ersetze hier die entsprechende "_____" stelle mit dem char element (char toString oder so?)
+            }
+
+            //System.out.println("test: " + charArray[i]);
+
+        }
+        return charArray;
     }
 
-    public String userInput(){
-      Scanner input = new Scanner(System.in);
-      for(int i = 6; i > 0; i--){
-          System.out.println("Enter city name (" + i + " tries left)");
-//          if(input.nextLine()==){
-//              input.nextLine();
-//          }
+//    public void foundChar(char[] cityChar) {
+//        if (cityChar.contains(foundChar)) {
+//
+//        }
+//    }
 
-      }
-      return input.nextLine();
+    public int letterCount() {
+        int count = 0;
+        for (int i = count; i < randomCityGenerator().length(); i++) {
+            count++;
+        }
+        return count;
+        //wie verbinde ich zeile 39, die anzahl der arrays elemente mit dieser methode
     }
+
+    public int triesLeft() {
+        int triesLeft = letterCount() * 2;
+        return triesLeft;
+    }
+
+    public void emptyWordLine() { // _o_____
+        System.out.println("Guess the city with one letter at a time: ");
+        for (int i = 0; i < getCurrentCity().length(); i++) {
+            System.out.print("_");
+        }
+        System.out.println("\n");
+
+
+        //for(int i = 0; i < city.length; i++){
+        //    city[i] =
+        //}
+        //TODO: shortcuts selber zuweisen
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
+    }
+
+    public void setCurrentCity(String currentCity) {
+        this.currentCity = currentCity;
+    }
+
+    public void startGame() {
+        System.out.println("CITY GUESSER");
+        createRandomCity();
+        printCurrentCity();
+        //gr.cityStringToCharArray();
+        emptyWordLine();
+        userInput();
+    }
+
 
 }
--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -4,11 +4,7 @@ package de.quinscape;
 public class Main {
     public static void main(String[] args) {
         Gluecksrad gr = new Gluecksrad();
-        gr.random();
-        //gr.triesLeft(6);
-
-
+        gr.startGame();
+        //gr.foundChar(gr.cityStringToCharArray());
     }
-
-
 }
