190208c7c041da054444a480000063b0ba8471d9
pchitanta <patrick.chitanta@quinscape.de>
2021-11-08 14:59:33 +0100

    Player class added

--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -3,6 +3,8 @@ package de.quinscape;
 public class Main {
     public static void main(String[] args) {
         Gluecksrad gr = new Gluecksrad();
+        Player player = new Player();
+        player.setPlayerName();
         gr.startGame();
     }
 }
--- src/de/quinscape/Player.java
+++ src/de/quinscape/Player.java
@@ -1,4 +1,12 @@
 package de.quinscape;
 
+import java.util.Scanner;
+
 public class Player {
+    private String playerName;
+    private Scanner sc = new Scanner (System.in);
+
+    public void setPlayerName() {
+        this.playerName = sc.nextLine();
+    }
 }
